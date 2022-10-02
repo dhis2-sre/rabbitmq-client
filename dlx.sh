97528fd71a079e6eaf6ce561ab9339f85e650b2e
teleivo <teleivo@users.noreply.github.com>
2022-10-02 06:42:43 +0200

    chore(wip):

--- dlx.sh
+++ dlx.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# create dead letter queue
+docker exec -it rabbitmq-rabbitmq-1 \
+rabbitmqadmin declare queue --vhost / \
+    name=dlx_queue_client_example durable=true
 
+# route from queue to dead letter queue
 docker exec -it rabbitmq-rabbitmq-1 \
 rabbitmqctl set_policy --vhost / \
     Q_TTL_DLX \
