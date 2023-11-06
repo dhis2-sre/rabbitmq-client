0b4369f6ff06ba1b6e6b65983102eda950347e13
Andreas Jensen <andreas@dhis2.org>
2023-11-06 21:02:38 +0700

    chore: add producer test

---
+++ pgk/queue/producer_test.go
@@ -0,0 +1,63 @@
+package queue
+
+import (
+	"fmt"
+	"testing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/orlangure/gnomock"
+	"github.com/orlangure/gnomock/preset/rabbitmq"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPreset(t *testing.T) {
+	t.Parallel()
+
+	// Given
+	p := rabbitmq.Preset(
+		rabbitmq.WithUser("guest", "guest"),
+	)
+	container, err := gnomock.Start(p)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, gnomock.Stop(container)) }()
+	uri := fmt.Sprintf("amqp://%s:%s@%s", "guest", "guest", container.DefaultAddress())
+
+	// When
+	producer := ProvideProducer(uri)
+	payload := struct{ ID uint }{uint(123)}
+	producer.Produce("ttl-destroy", payload)
+
+	// Then
+	conn, err := amqp.Dial(uri)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, conn.Close()) }()
+
+	ch, err := conn.Channel()
+	require.NoError(t, err)
+	defer func() { require.NoError(t, ch.Close()) }()
+
+	q, err := ch.QueueDeclare(
+		"ttl-destroy",
+		false, // Durable
+		false, // Delete when unused
+		false, // Exclusive
+		false, // No-wait
+		nil,   // Arguments
+	)
+	require.NoError(t, err)
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	require.NoError(t, err)
+
+	m := <-msgs
+	require.Equal(t, []byte(`{"ID":123}`), m.Body)
+}
